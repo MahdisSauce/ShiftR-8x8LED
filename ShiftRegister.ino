af5bfc991832b02550c6982e52cf79db2b39c9fd
Mahdi Fawaz <97362497+Epicsnoozebear@users.noreply.github.com>
2022-05-04 13:12:46 -0400

    Add files via upload

--- ShiftRegister.ino
+++ ShiftRegister.ino
@@ -1,8 +1,43 @@
+byte * reverseBitOrder(byte img[]){ //reverses bit order for each byte of img
+    for(int i = 0; i < 8; i++){
+        img[i] = (img[i] & 0xF0) >> 4 | (img[i] & 0x0F) << 4;
+        img[i] = (img[i] & 0xCC) >> 2 | (img[i] & 0x33) << 2;
+        img[i] = (img[i] & 0xAA) >> 1 | (img[i] & 0x55) << 1;
+    }
+    return img;
+}
+
+byte * frameShift(byte img[], bool dir, bool axis, int offset){ //returns img with proper offset
+    byte* holder = new byte[8];
+    for(int x = 0; x < 8; x++){
+        if(dir == 0){
+            if(axis == 0)
+                holder[x] = img[x] >> offset;
+            else    
+                if(x + offset >= 8)
+                    holder[x] = 0;
+                else
+                    holder[x] = img[x + offset];
+        }
+        else
+            if(axis == 0)
+                holder[x] = img[x] << offset;
+            else
+                if(x-offset < 0)
+                    holder[x] = 0;
+                else
+                    holder[x] = img[x-offset];  
+    }
+    return holder;
+}
+
+
+
 class ledMatrix{
     public:
-        int dataPin;        //Defining variables to be used for the pins
-        int clockPin;
-        int latchPin;
+        int dataPin;        //Pin 14 On the Shift Register
+        int clockPin;       //Pin 11 On the Shift Register
+        int latchPin;       //Pin 12 On the Shift Register
         unsigned int prevMillis;    //Defining variable to be used for timing
         unsigned int currMillis;
 
@@ -24,10 +59,10 @@ class ledMatrix{
 
         void toDisplay(byte img[]){     //Displaying image bases on 8 Byte array sent to function
             for(int i = 0; i < 8; i++){
-                digitalWrite(latchPin, LOW);
-                shiftOut(dataPin, clockPin, LSBFIRST, ~img[i]);
-                shiftOut(dataPin, clockPin, MSBFIRST, (1 << i));
-                digitalWrite(latchPin, HIGH);
+                digitalWrite(latchPin, LOW);                    //Prepares pin for rising edge
+                shiftOut(dataPin, clockPin, LSBFIRST, ~img[i]); //Displays the current byte
+                shiftOut(dataPin, clockPin, MSBFIRST, (1 << i));//Turns on pin for current byte
+                digitalWrite(latchPin, HIGH);                   //Shifts data from shift register to output register
                 delay(1);
                 }
             clearDisplay();
@@ -42,74 +77,39 @@ class ledMatrix{
                 toDisplay(img);
             };
         }
+        void fromSide(byte img[], bool dir){ //Pans image from left or right off screen
+            for(int i = 8; i >= 0; i--){
+                timedDisplay(frameShift(img, !dir, 0, i), 300);
+            }
+        }
 
+        
         void panDisplay(byte img[], bool dir){  //Pans the image left or right until off screen starting from the center
             for(int i = 0; i <= 8; i++){        //For loop to determine how far it is moved each frame
-                timedDisplay(verticalShift(img, dir, i), 300);
+                timedDisplay(frameShift(img, dir, 0, i), 300);
             }   
         }
         void ventDisplay(byte img[],bool dir){  //Pans image up or down until off screen starting from center
                 for(int i = 0; i <= 8; i++){  
-                    timedDisplay(verticalShift(img, dir, i), 300);
+                    timedDisplay(frameShift(img, dir, 1, i), 300);
                 }
-        }
-        
-        void fromSide(byte img[], bool dir){ //Pans image from left or right off screen
-            for(int i = 8; i >= 0; i--){
-                timedDisplay(horizontalShift(img, dir, i), 300);
-            }
         }
 
         void fromVertical(byte img[], bool dir){          
             for(int i = 8; i >= 0; i--){  
-                timedDisplay(verticalShift(img, dir, i), 300);
+                timedDisplay(frameShift(img, !dir, 1,  i), 300);
             }
-        }
-
-        byte * verticalShift(byte img[], bool dir, int offset){
-            byte* holder = new byte[8];
-            for(int x = 0; x < 8; x++){
-                if(dir == 0){
-                    if(x + offset >= 8)
-                        holder[x] = 0;
-                    else
-                        holder[x] = img[x + offset];
-                }
-                else
-                    if(x-offset < 0)
-                        holder[x] = 0;
-                    else
-                        holder[x] = img[x-offset];  
-            }
-            return holder;
-        }
-
-        byte * horizontalShift(byte img[], bool dir, int offset){
-            byte* holder = new byte[8];    
-            for(int x = 0; x < 8; x++){
-                if(dir == 1)
-                    holder[x] = img[x] << offset;
-                else
-                    holder[x] = img[x] >> offset;
-            }
-            return holder;
         }
 
 };
+
+
 
 // Maps of letters and images
 byte ALL[] = {B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111};
 byte BOX[] = {B11111111,B11111111,B11000011,B11000011,B11000011,B11000011,B11111111,B11111111};
 byte CREEPER[] = {B11111111, B10011001, B10011001, B11100111, B11000011, B11000011, B11011011, B11111111};
-byte AMOGUS[] = {
-    B01111100,
-    B11111110,
-    B00011111,
-    B00011111,
-    B11111111,
-    B11111110,
-    B11101110,
-    B11101110};
+byte AMOGUS[] = {B01111100,B11111110,B00011111,B00011111,B11111111,B11111110,B11101110,B11101110};
 byte EX[] = {B00000000,B00010000,B00010000,B00010000,B00010000,B00000000,B00010000,B00000000};
 //byte A[] = {B00000000,B00111100,B01100110,B01100110,B01111110,B01100110,B01100110,B01100110};
 //byte B[] = {B01111000,B01001000,B01001000,B01110000,B01001000,B01000100,B01000100,B01111100};
@@ -143,17 +143,17 @@ ledMatrix mat1(3, 4, 2);
 
 void setup() {
     // put your setup code here, to run once:
+      
 }
 
 
 void loop() {
     // put your main code here, to run repeatedly:
-    mat1.fromSide(AMOGUS, 0); //from right to left
-    mat1.panDisplay(AMOGUS, 0);//from center to up
-    mat1.ventDisplay(AMOGUS, 0);
-    mat1.fromVertical(AMOGUS, 0);
     
-
+    byte* reverse = new byte[8];
     
+    reverse = reverseBitOrder(AMOGUS);
+    
+    mat1.timedDisplay(reverse, 500);
 
 }
