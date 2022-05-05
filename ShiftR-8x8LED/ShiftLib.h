4c2e00061fca1dbaf443e288e7dc6aa421cc1f26
MahdisSauce <97362497+MahdisSauce@users.noreply.github.com>
2022-05-05 16:30:01 -0400

    Update ShiftLib.h

--- ShiftR-8x8LED/ShiftLib.h
+++ ShiftR-8x8LED/ShiftLib.h
@@ -10,15 +10,15 @@ byte * frameShift(byte img[], bool dir, bool axis, int offset); //returns img wi
 
 class ledMatrix{
     private:
-        short dataPin;        //Pin 14 On the Shift Register
-        short clockPin;       //Pin 11 On the Shift Register
-        short latchPin;       //Pin 12 On the Shift Register
+        byte dataPin;        //Pin 14 On the Shift Register
+        byte clockPin;       //Pin 11 On the Shift Register
+        byte latchPin;       //Pin 12 On the Shift Register
     
     public:
         unsigned int prevMillis;    //Defining variable to be used for timing
         unsigned int currMillis;
         
-        ledMatrix(short x, short y, short z); //Initializes the shift register pins
+        ledMatrix(byte x, byte y, byte z); //Initializes the shift register pins
         
         void clearDisplay();    // Clears the display
 
